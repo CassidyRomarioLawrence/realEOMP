7203ee9ff51eafc5ed83d57777a5a543a0a0a109
CassidyRomarioLawrence <cassidyromario@gmail.com>
2022-12-09 16:25:58 +0200

    changes

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,6 @@
 let products = JSON.parse(localStorage.getItem('items'))
 
-function tableDisplay() {
+function displayed() {
     products.forEach((items) => {
         document.querySelector('.table').innerHTML += `
         <tr>
@@ -11,10 +11,27 @@ function tableDisplay() {
             <td>R ${items.description.price}</td>
             <td>
             <i onClick="" class="fa-solid fa-pen-to-square" data-bs-toggle="modal" data-bs-target="#Modal1"></i> 
-            <i onClick="" class="fa-solid fa-trash"></i>
+            <i onClick="deleteIt()" class="fa-solid fa-trash"></i>
             </td>
         </tr>`
     })
 };
 
-tableDisplay();
+displayed();
+
+let add = document.querySelector('#addProduct');
+
+add.addEventListener('click', () => {
+    let name = document.querySelector('#productName').value;
+    let image = document.querySelector('#image').value;
+    let size = document.querySelector('#productSize').value;
+    let color = document.querySelector('#productColor').value;
+
+    products.push(
+        {
+            
+        }
+    )
+})
+
+
